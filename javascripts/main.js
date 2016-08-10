01f82d26e7e6fc38206c66750c206b73f19b5dba
Shirley0926 <chaofan0926@163.com>
2016-08-10 16:38:02 +0800

    Display the task in html directory

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
 console.log('This would be the main JS file.');
+window.onload=function(){
+    _init();
+};
+function _init(){
+    var ul = document.getElementById("uldemo4js");
+    for(i=13;i<=20;i++)
+    {
+        var task_name = "task"+i;
+        var task_path = "html/"+task_name+".html";
+        var li = "<li><a href='"+task_path+"'>"+task_name+"</a></li>"
+        ul.innerHTML+=li;
+    }
+    console.log(ul);
+
+}
