aa853f394f212767620c8e4b14b698fa42ca8997
Shirley Yang <chaofan0926@163.com>
2016-08-31 20:11:56 +0800

    task 26 complete!

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,11 +4,18 @@ window.onload=function(){
 };
 function _init(){
     var ul = document.getElementById("uldemo4js");
-    for(i=13;i<=32;i++)
+    for(i=13;i<=20;i++)
     {
         var task_name = "task"+i;
         var task_path = "html/"+task_name+".html";
-        var li = "<li><a href='"+task_path+"'>"+task_name+"</a></li>"
+        var li = "<li><a href='"+task_path+"'>"+task_name+"</a></li>";
+        ul.innerHTML+=li;
+    }
+    for(i=21;i<=36;i++)
+    {
+        var task_name = "task"+i;
+        var task_path = task_name+"/"+task_name+".html";
+        var li = "<li><a href='"+task_path+"'>"+task_name+"</a></li>";
         ul.innerHTML+=li;
     }
     console.log(ul);
