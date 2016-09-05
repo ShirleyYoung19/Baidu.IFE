acc4a5883fdc9b01da28b4e4c0ec022e8eb3833c
Shirley Yang <chaofan0926@163.com>
2016-09-05 21:30:59 +0800

    发现飞船的能量系统有些问题,今天修正了一部分,其他的后续改进

---
+++ task28/consoleOut.js
@@ -0,0 +1,128 @@
+/**
+ * Created by ShirleyYang on 16/8/28.
+ * 用于控制console输出的模块
+ */
+var consoleOut={
+    consoleDiv:$(".console")[0],
+
+    //获得指挥官发出的信息
+    getCommand:function (BUS,success) {
+        var commandOrder=BUS.slice(2);
+        var id=parseInt(BUS.slice(0,2),2);
+        switch (commandOrder){
+            case "00":
+                this.create(success,id);
+                break;
+            case "01":
+                this.explode(success,id);
+                break;
+            case "10":
+                this.launch(success,id);
+                break;
+            case "11":
+                this.stop(success,id);
+
+        }
+
+    },
+
+    //如果是创建飞船命令:
+    create:function (success,id) {
+        var ele1,ele2;
+        var time=this.getTime();
+        //已发送出去
+        if(success!=undefined){
+            if(success==true){
+                ele2="<p class='success'>"+time+"  [消息]创建"+(id+1)+"号飞船成功了!</p>";
+            }
+            else{
+                ele2="<p>"+time+"  [注意]创建"+(id+1)+"号飞船命令丢包了!!!正在重试...</p>";
+            }
+            $(this.consoleDiv).append(ele2);
+            this.scrollBottom();
+        }else{  //指挥官命令
+            ele1="<p class='success'>"+time+"  [指挥官]:创建"+(id+1)+"号飞船命令已发送</p>";
+            $(this.consoleDiv).append(ele1);
+            this.scrollBottom();
+        }
+    },
+
+    //自爆飞船命令
+    explode:function (success,id) {
+        var ele1,ele2;
+        var time=this.getTime();
+        if(success!=undefined) {
+            if (success) {
+                ele2 = "<p class='success'>" + time+ "  [消息]自爆" + (id + 1) + "号飞船成功了!</p>";
+            }
+            else {
+                ele2 = "<p>" + time + "  [注意]自爆" + (id + 1) + "号飞船命令丢包了!!!,正在重试...</p>";
+            }
+            $(this.consoleDiv).append(ele2);
+            this.scrollBottom();
+        }else {
+            ele1="<p class='success'>"+time+"  [指挥官]:自爆"+(id+1)+"号飞船命令已发送</p>";
+            $(this.consoleDiv).append(ele1);
+            this.scrollBottom();
+        }
+
+
+
+    },
+
+    //飞船启动命令
+    launch:function (success,id) {
+        var ele1, ele2;
+        var time = this.getTime();
+        if (success != undefined) {
+            if (success) {
+                ele2 = "<p class='success'>" + time + "  [消息]启动" + (id + 1) + "号飞船成功了!</p>";
+            } else {
+                ele2 = "<p>" + time + "  [注意]启动" + (id + 1) + "号飞船命令丢包了!!!正在重试...</p>";
+            }
+            $(this.consoleDiv).append(ele2);
+            this.scrollBottom();
+        } else {
+            ele1 = "<p class='success'>" + time + "  [指挥官]:启动" + (id + 1) + "号飞船命令已发送</p>";
+            $(this.consoleDiv).append(ele1);
+            this.scrollBottom();
+        }
+    },
+    //飞船停飞命令
+    stop:function (success,id) {
+        var ele1,ele2;
+        var time=this.getTime();
+        if(success != undefined){
+            if(success){
+                ele2="<p class='success'>"+time+"  [消息]停飞"+(id+1)+"号飞船成功了!</p>";
+            }
+            else{
+                ele2="<p>"+time+"  [注意]停飞"+(id+1)+"号飞船命令丢包了!!!正在重试...</p>";
+            }
+            $(this.consoleDiv).append(ele2);
+            this.scrollBottom();
+        }else{
+            ele1="<p class='success'>"+time+"  [指挥官]:停飞"+(id+1)+"号飞船命令已发送</p>";
+            $(this.consoleDiv).append(ele1);
+            this.scrollBottom();
+        }
+    },
+
+    //保证console输出界面scrollBar一直在画面最下方
+    scrollBottom:function () {
+     //   var scrollBottom=$(this.consoleDiv).scrollTop()+$(this.consoleDiv).height();
+        $(this.consoleDiv).scrollTop(this.consoleDiv.scrollHeight);
+    },
+    getTime:function () {
+        var time=new Date();
+        var milliseconds=time.getMilliseconds().toString();
+        if(milliseconds.length<3){
+            if(milliseconds.length==2){
+                milliseconds="0"+milliseconds;
+            }else {
+                milliseconds="00"+milliseconds;
+            }
+        }
+        return time.toLocaleTimeString()+":"+milliseconds;
+    }
+};
--- task28/craft.js
+++ task28/craft.js
@@ -9,8 +9,7 @@ function Craft(id,power,energy) {
     this.state='';
     this.id=id;
     this.speed=power[0];
-    this.timer='';
-    this.timerStop='';//这个timer变量是监视在launch时的时间间隔函数
+    this.timer='';//这个timer变量是监视在launch时的时间间隔函数
 }
 Craft.prototype.create=function () {
     this.state="stop";
@@ -47,18 +46,19 @@ Craft.prototype.getCommand=function (BUS) {
                 break;
             case "launch":
                 this.state="launch";
-                this.timer=this.launch(orbite,this.timerStop);
+                this.timer=this.launch(orbite);
                 break;
             case "stop":
                 this.state='stop';
-                this.timerStop=this.stop(orbite,this.timer);
+                this.stop(orbite,this.timer);
                 break;
 
         }
 
     }
+    parseInt()
 };
-Craft.prototype.launch=function (orbite,timer) {
+Craft.prototype.launch=function (orbite) {
     var deg;
     var pattern=/\d{1,3}[.]\d{3}/;  //设定一个正则表达式
     var speed=parseFloat((this.speed*0.1*360/(230+this.id*80)/2/Math.PI).toFixed(3));
@@ -67,33 +67,25 @@ Craft.prototype.launch=function (orbite,timer) {
     var energyNumber=$(craftInner).children().first();
     var energyBar=$(craftInner).children().last();
     var obj=this;
-    //如果之前是停飞状态要先将停飞状态的时间间隔函数取消
-    if(timer){
-        clearInterval(timer);
-    }
     if($(craft).css("transform")=="none"){ //检查一下craft div是不是已经有了transform属性
         deg=0;  //没有的话将角度设置为0
     }else {
         deg=parseFloat(pattern.exec($(craft).attr("style"))[0]); //若已有transform属性,则将其值提取出来,注意转换成数字
     }
     var timerLaunch=setInterval(function () {
-        if(!obj.timer){
-            obj.timer=timer;
-        }
         $(craft).css("transform","rotate("+deg+"deg)");//设定飞船飞行动画
         deg=deg+speed;//每隔0.1s增加一定的角度
         var energyText=obj.getEnergy(obj.state);//获取能量值
-        // if(energyText=="-0"){
-        //     energyText=0;
-        // }
+        if(energyText=="-0"){
+            energyText=0;
+        }
         $(energyNumber).text(energyText);//将获得的能量值反应到飞船上
         $(energyBar).css("height",0.4*energyText+"px");//将获得的能量值反应到飞船的能量条上
         if(energyText<=30){
             $(energyBar).css("background-color","rgb(200,59,56)");//若能量低于30%,提示变红
         }
         if(energyText==0){
-            // clearInterval(timerLaunch);
-            // $(".craft-orbit"+(obj.id+1)+" input[name='launch']").val("飞行");
+            clearInterval(timerLaunch);
             obj.stop(orbite);//将状态更改为stop开始调用stop时的命令
         }
     },100);
@@ -111,9 +103,6 @@ Craft.prototype.stop=function (orbite,timer) {
     var energyBar=$(craftInner).children().last();
     var obj=this;
     var timerStop=setInterval(function () {
-        if(!obj.timerStop){
-            obj.timerStop=timerStop;
-        }
         var energyText=obj.getEnergy(obj.state);//获取能量值
         if(energyText=="-0"){
             energyText=0;
@@ -128,19 +117,17 @@ Craft.prototype.stop=function (orbite,timer) {
         if(energyText==100){
             clearInterval(timerStop);
         }
-    },100);
-    return timerStop;
+    },100)
 };
 
 
 
 Craft.prototype.getEnergy=function (state) {
     if(state=="launch"){//如果状态是飞行
-        this.energy=this.energy-(this.energyConsume-this.energyAdd)*0.1;
-        if(this.energy<0){
+        if(this.energy>0){
+            this.energy=this.energy-(this.energyConsume-this.energyAdd)*0.1
+        }else{
             this.energy=0;
-            // 能量变为0的时候,将飞行的控制器的时间时间取消
-            clearInterval(this.timer);
             this.state='stop';
             var launchBtn=$("[class|='craft']").get(this.id-1);//获得这个飞船对应的飞行按钮
             $(launchBtn.lastElementChild).val("飞行");
